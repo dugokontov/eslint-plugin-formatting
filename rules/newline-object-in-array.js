f58eff03a62bbe8d4f851bcd77f3a3690dc289bb
Sergej Dugokontov <sergejspam@gmail.com>
2015-06-19 11:56:05 +0200

    No error if object literal is in one line

--- rules/newline-object-in-array.js
+++ rules/newline-object-in-array.js
@@ -8,7 +8,7 @@ module.exports = function(context) {
         ObjectExpression: function (node) {
             var parent = node.parent;
             if (parent.type === 'ArrayExpression') {
-                if (parent.loc.start.line !== parent.loc.end.line) {
+                if (parent.loc.start.line !== parent.loc.end.line && node.loc.start.line !== node.loc.end.line) {
                     if (node.loc.start.line === parent.loc.start.line) {
                         // console.log(node.loc, parent.loc);
                         context.report(node, node.loc.start, SHOULD_START_IN_NEW_LINE);
